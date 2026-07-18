0f609042dfe8fdee12d2a1351296ab8a7e931ede
e273727 <rohitomni2125@gmail.com>
2026-07-18 23:56:18 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.py
+++ array/3sum.py
@@ -3,12 +3,13 @@ class Solution:
         ans=set()
         n = len(nums)
         for i in range(0, n):
-            for j in range(i+1,n):
-                for k in range(j+1,n):
-                    if (nums[i]+nums[j]+nums[k]==0):
-                        listAns = [nums[i], nums[j], nums[k]]
-                        listAns.sort()
-                        ans.add(tuple(listAns))
+            for j in range(i+1,n-1):
+                k=j+1
+                if (nums[i]+nums[j]+nums[k]==0):
+                    listAns = [nums[i], nums[j], nums[k]]
+                    listAns.sort()
+                    ans.add(tuple(listAns))
+                k+=1
         return list(ans)
 
         
