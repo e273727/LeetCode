6575a5614e62e1c56fc1237e83cce640a133482b
e273727 <rohitomni2125@gmail.com>
2026-07-23 21:26:56 +0530

    LC-sort-colors [Runtime: 0 ms; Memory: 19.2 MB]

--- two-pointers/sort-colors.py
+++ two-pointers/sort-colors.py
@@ -3,22 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        low = 0
-        mid = 0
-        high = len(nums)-1
+        low, mid, high = 0, 0, len(nums)-1
         while mid<=high:
             if nums[mid] == 0:
-                temp = nums[low]
-                nums[low] = nums[mid]
-                nums[mid]=temp
+                nums[low], nums[mid] = nums[mid], nums[low]
                 low+=1
                 mid+=1
             elif nums[mid] == 1:
                 mid+=1
             elif nums[mid] == 2:
-                temp = nums[high]
-                nums[high] = nums[mid]
-                nums[mid]=temp
+                nums[high], nums[mid] = nums[mid], nums[high]
                 high-=1
 
         
