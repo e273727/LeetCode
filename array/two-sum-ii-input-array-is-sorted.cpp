d289e7802d9839ce6542ccd2d2ce7a8e7175ce12
e273727 <rohitomni2125@gmail.com>
2026-08-05 21:53:15 +0530

    LC-two-sum-ii-input-array-is-sorted [Runtime: N/A; Memory: N/A]

--- array/two-sum-ii-input-array-is-sorted.cpp
+++ array/two-sum-ii-input-array-is-sorted.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int> ans;
-        for(int i = 0; i<numbers.size(); i++)
-        {
-            for(int j =1; j<numbers.size(); j++)
-            {
-                if ((numbers.at(i)+numbers.at(j) == target)&&(numbers.at(i)!=numbers.at(j)))
-                {
-                    ans.emplace_back(i+1);
-                    ans.emplace_back(j+1);
-                    return ans;
-                }
+        int right = 0;
+        int left = numbers.size()-1;
+        vector<int> ans(2,0);
+        while (right<left){
+            int sum = numbers.at(right)+numbers.at(left);
+            if (sum == target){
+                ans.at(0) = right+1;
+                ans.at(1) = left+1;
+                return ans;
+            }
+            else if(sum>target){
+                left--;
+            }
+            else{
+                right++;
             }
         }
-        return {};
+        return {};        
     }
 };
