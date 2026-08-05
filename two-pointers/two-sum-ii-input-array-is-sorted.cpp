ff5d2f86c43e4df4b9114c8182a143a50e86dd93
e273727 <rohitomni2125@gmail.com>
2026-08-05 21:31:23 +0530

    LC-two-sum-ii-input-array-is-sorted [Runtime: N/A; Memory: N/A]

--- two-pointers/two-sum-ii-input-array-is-sorted.cpp
+++ two-pointers/two-sum-ii-input-array-is-sorted.cpp
@@ -6,7 +6,7 @@ public:
         {
             for(int j =1; j<numbers.size(); j++)
             {
-                if (numbers.at(i)+numbers.at(j) == target)
+                if ((numbers.at(i)+numbers.at(j) == target)&&(numbers.at(i)!=numbers.at(j)))
                 {
                     ans.emplace_back(i+1);
                     ans.emplace_back(j+1);
