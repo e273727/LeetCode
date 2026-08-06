11f9551a481dd19d5208f0fb51f24b86dac6e997
e273727 <rohitomni2125@gmail.com>
2026-08-06 20:40:47 +0530

    LC-smallest-divisible-digit-product-i [Runtime: N/A; Memory: N/A]

---
+++ math/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int digitProduct(int n){
+        int product = 1;
+        while(n!=0){
+            int digit = n%10;
+            product*=digit;
+            n/=10;            
+        }
+        return product;
+    }
+    int smallestNumber(int n, int t) {
+        int digit;
+        while (n<100){
+            int product = digitProduct(n);
+            if(product%t == 0){
+                return n;
+            }
+            else{
+                n++;
+            }
+        }
+        return -1;        
+    }
+};
