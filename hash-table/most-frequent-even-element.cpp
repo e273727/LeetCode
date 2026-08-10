37602a2e8ae615b71b73e722bcf9ce2fecaf5980
e273727 <rohitomni2125@gmail.com>
2026-08-10 21:32:17 +0530

    LC-most-frequent-even-element [Runtime: 27 ms; Memory: 41.8 MB]

--- hash-table/most-frequent-even-element.cpp
+++ hash-table/most-frequent-even-element.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
-        int el;
-        int prev;
-        int count =0;
-        for(int i = 0;i<nums.size();i++){
-            if(count == 0 && nums.at(i)%2 == 0){
-                count++;
-                el = nums.at(i);
-            }
-            else if(nums.at(i)%2!=0){
-                continue;
-            }
-            else if(nums.at(i) == el){
-                count++;
-            }
-            else{
-                count--;
-                if(count == 0){
-                    prev = el;
-                }
+        unordered_map<int, int> freq;
+        for(int num: nums){
+            if(num%2 == 0){
+                freq[num]++;
             }
         }
-        if(prev<el){
-            return prev;
+        int ans = -1;
+        int maxFreq = 0;
+        for(auto& [num,count] : freq){
+            if(count>maxFreq || (count == maxFreq && num<ans)){
+                maxFreq = count;
+                ans = num;
+            }
         }
-        else{
-            return el;
-        }
-        return -1;
+        return ans;        
     }
 };
