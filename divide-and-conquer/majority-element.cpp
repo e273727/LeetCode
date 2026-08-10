9c2e9267947129f5522cd8cd1e9b0691f3394b00
e273727 <rohitomni2125@gmail.com>
2026-08-10 20:42:53 +0530

    LC-majority-element [Runtime: N/A; Memory: N/A]

---
+++ divide-and-conquer/majority-element.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int el;
+        for(int i = 0; i<nums.size();i++){
+            if(count == 0){
+                count++;
+                el = nums[i];
+            }
+            else if(el == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        int count1 =0;
+        for(int i = 0; i<nums.size(); i++){
+            if(el == nums[i]){
+                count1++;
+            }
+        }
+        if(count1>nums.size()/2){
+            return el;
+        }
+        return -1;
+    }
+};
