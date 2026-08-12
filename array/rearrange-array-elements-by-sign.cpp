f3209de2cdd97052d54bdb79df849353875834c1
e273727 <rohitomni2125@gmail.com>
2026-08-12 23:42:42 +0530

    LC-rearrange-array-elements-by-sign [Runtime: N/A; Memory: N/A]

--- array/rearrange-array-elements-by-sign.cpp
+++ array/rearrange-array-elements-by-sign.cpp
@@ -3,23 +3,29 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
         vector<int> minus;
         vector<int> plus;
+        int j =0;
+        int k =0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]<0){
                 minus.push_back(nums[i]);
-            }
-            else{
-                plus.push_back(nums[i]);
-            }
-        }
-        int j =0;
-        int k =0;
-        for(int i =0;i<nums.size();i++){
-            if(i%2!=0){
-                nums[i] == minus[j];
+                cout<<"minus wale: "<<minus[j]<<endl;;
                 j++;
             }
             else{
-                nums[i] == plus[k];
+                plus.push_back(nums[i]);
+                cout<<"plus wale: "<<plus[k]<<endl;;
+                k++;
+            }
+        }
+        j =0;
+        k =0;
+        for(int i =1;i<nums.size()+1;i++){
+            if(i%2!=0){
+                nums[i-1] = plus[j];
+                j++;
+            }
+            else{
+                nums[i-1] = minus[k];
                 k++;
             }
         }
