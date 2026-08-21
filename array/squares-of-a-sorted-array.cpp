35b0771287bf428ee03528af4e6a373d652e65f5
e273727 <rohitomni2125@gmail.com>
2026-08-21 21:31:51 +0530

    LC-squares-of-a-sorted-array [Runtime: N/A; Memory: N/A]

--- array/squares-of-a-sorted-array.cpp
+++ array/squares-of-a-sorted-array.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i =0; i<nums.size(); i++){
-            nums[i] = nums[i]*nums[i];
+        int n = nums.size();
+        vector<int> result(n,0);
+        int i = 0;
+        int j = n-1;
+        for(int p = n-1;p>=0; p--){
+            if(abs(nums[i])>abs(nums[j])){
+                result[p]=nums[i]*nums[i];
+                i++;
+            }
+            else{
+                result[p]=nums[j]*nums[j];
+                j--;
+            }
         }
-        sort(nums.begin(), nums.end());
-        return nums;      
+        return result;      
     }
 };
