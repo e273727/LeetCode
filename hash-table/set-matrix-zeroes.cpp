bf5471924ee50b8742b179827024d881526db71f
e273727 <rohitomni2125@gmail.com>
2026-08-23 21:53:31 +0530

    LC-set-matrix-zeroes [Runtime: N/A; Memory: N/A]

---
+++ hash-table/set-matrix-zeroes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void markRow(vector<vector<int>>& matrix, int i){
+        for(int j = 0; j<matrix[0].size(); j++){
+            if(matrix[i][j]!=0){
+                matrix[i][j] = -1;
+            }
+        }
+    };
+    void markColumn(vector<vector<int>>& matrix, int j){
+        for(int i =0; i<matrix.size(); i++){
+            if(matrix[i][j]!=0){
+                matrix[i][j] = -1;
+            }
+        }
+    };
+    void setZeroes(vector<vector<int>>& matrix) {
+        for(int i = 0; i<matrix.size(); i++){
+            for(int j =0; j<matrix[0].size(); j++){
+                if(matrix[i][j] == 0){
+                    markRow(matrix, i);
+                    markColumn(matrix, j);
+                }
+            }
+        }        
+        for(int i = 0; i<matrix.size(); i++){
+            for(int j =0; j<matrix[0].size(); j++){
+                if(matrix[i][j] == -1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }        
+    }
+};
