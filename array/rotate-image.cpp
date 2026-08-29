16cc1cea9113fedd66b0cc1fb0e8af2713aec72f
e273727 <rohitomni2125@gmail.com>
2026-08-29 23:16:24 +0530

    LC-rotate-image [Runtime: N/A; Memory: N/A]

--- array/rotate-image.cpp
+++ array/rotate-image.cpp
@@ -2,16 +2,28 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> answer(n,vector<int>(n,0));
-        for(int i = 0;i<n; i++){
-            for(int j =0; j<n; j++){
-                answer[j][n-1-i] = matrix[i][j];
+    //     vector<vector<int>> answer(n,vector<int>(n,0));
+    //     for(int i = 0;i<n; i++){
+    //         for(int j =0; j<n; j++){
+    //             answer[j][n-1-i] = matrix[i][j];
+    //         }
+    //     }        
+    //     for(int i = 0;i<n; i++){
+    //         for(int j =0; j<n; j++){
+    //             matrix[i][j] = answer[i][j];
+    //         }
+    //     }        
+    // }
+    // By the Transposing matrix
+        for(int i =0; i<n; i++){
+            for(int j =i; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
-        }        
-        for(int i = 0;i<n; i++){
-            for(int j =0; j<n; j++){
-                matrix[i][j] = answer[i][j];
-            }
-        }        
+        }
+        for(auto& row: matrix){
+            reverse(row.begin(), row.end());
+        }
     }
 };
