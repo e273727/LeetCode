74612b080645ef13d6a6a3c4a207f7f0589b98b9
e273727 <rohitomni2125@gmail.com>
2026-09-03 22:41:57 +0530

    LC-contains-duplicate [Runtime: N/A; Memory: N/A]

--- array/contains-duplicate.cpp
+++ array/contains-duplicate.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i =1; i<nums.size(); i++){
-            if(nums[i] == nums[i-1]){
+        // sort(nums.begin(), nums.end());
+        // for(int i =1; i<nums.size(); i++){
+        //     if(nums[i] == nums[i-1]){
+        //         return true;
+        //     }
+        // }
+        // return false;       
+        // by the help of the sorting is the better appraoch, with the hashmap the approach will be much better ig
+        unordered_set<int> map;
+        for(int num: nums){
+            if(map.count(num)>0){
                 return true;
             }
+            map.insert(num);
         }
-        return false;        
+        return false;
     }
 };
