e2faa0178379e3f364e839d87ed510f411e373a3
e273727 <rohitomni2125@gmail.com>
2026-09-04 22:05:57 +0530

    LC-smallest-stable-index-i [Runtime: N/A; Memory: N/A]

--- array/smallest-stable-index-i.cpp
+++ array/smallest-stable-index-i.cpp
@@ -23,20 +23,23 @@ public:
         // return -1;   
         // this is was my method by the brute force method, ig there can be a better way, where we are able to keep track of max and min for every index, 
         int n = nums.size();
-        vector<int> maxx(n), minn(n);
-        maxx[0] = nums[0];
+        vector<int> minn(n);
+        int maxx = nums[0];
         minn[n - 1] = nums[n - 1];
 
-        for (int i = 1; i < n; i++) {
-            maxx[i] = max(maxx[i - 1], nums[i]);
-        }
+        // for (int i = 1; i < n; i++) {
+        //     maxx[i] = max(maxx[i - 1], nums[i]);
+        // }
 
         for (int i = n - 2; i >= 0; i--) {
             minn[i] = min(minn[i + 1], nums[i]);
         }
 
         for (int i = 0; i < n; i++) {
-            if (maxx[i] - minn[i] <= k)
+            if(maxx<nums[i]){
+                maxx = nums[i];
+            }
+            if (maxx - minn[i] <= k)
                 return i;
         }
 
