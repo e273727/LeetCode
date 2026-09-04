f92836cbe46b2194b1d7afaa61e64db1b1f09630
e273727 <rohitomni2125@gmail.com>
2026-09-04 21:28:17 +0530

    LC-smallest-stable-index-i [Runtime: N/A; Memory: N/A]

---
+++ prefix-sum/smallest-stable-index-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int max = nums[0];
+        int minStable = -1;
+        for(int i =0; i<nums.size(); i++){
+            int min = nums[i];
+            if(max<=nums[i]){
+                max = nums[i];
+                // cout<<max<<"\n";
+            }
+            for(int j = i; j<nums.size(); j++){
+                if(min>nums[j]){
+                    min = nums[j];
+                    // cout<<"min"<<min<<"\n";
+                }
+            }
+            int diff = max-min;
+            if(diff<=k){
+                minStable = i;
+            }
+        }
+        return minStable;        
+    }
+};
