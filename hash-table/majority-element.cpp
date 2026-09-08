43842db016d15839a6b0cdbc7f56f7eeee8a3a95
e273727 <rohitomni2125@gmail.com>
2026-09-08 21:32:35 +0530

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- hash-table/majority-element.cpp
+++ hash-table/majority-element.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int element = nums[0];
+        int n = nums.size();
         int count = 0;
-        int el;
-        for(int i = 0; i<nums.size();i++){
+        for(int i =0; i<n i++){
             if(count == 0){
-                count++;
-                el = nums[i];
+                j = i;
+                element = nums[i]
             }
-            else if(el == nums[i]){
-                count++;
-            }
-            else{
+            else if(nums[i]!=nums[j]){
                 count--;
             }
+            else{
+                count++;
+            }
         }
-        int count1 =0;
-        for(int i = 0; i<nums.size(); i++){
-            if(el == nums[i]){
+        int count1 = 0;
+        for(int i =0; i<n; i++){
+            if(element == nums[i]){
                 count1++;
             }
         }
-        if(count1>nums.size()/2){
-            return el;
+        if(count1>n/2){
+            return element;
         }
-        return -1;
+        return -1;        
     }
 };
