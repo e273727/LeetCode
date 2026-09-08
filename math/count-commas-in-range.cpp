224dd5f58d32ef2b0c5c5512250d166ac69e657a
e273727 <rohitomni2125@gmail.com>
2026-09-08 21:03:14 +0530

    LC-count-commas-in-range [Runtime: N/A; Memory: N/A]

--- math/count-commas-in-range.cpp
+++ math/count-commas-in-range.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     int countCommas(int n) {
+        int count = 0;
         if(n<1000){
             return 0;
         }
-        int last = n%100;
-        return last+1;
+        int digit = n;
+        do {
+        digit /= 10;
+        ++count;
+        } while (digit != 0);
+        int power = pow(10,count-1);
+        int second = n%power;
+        return second+1;
     }
 };
