f6d6a801ffbb244c670e4a59e21b432e146b1986
e273727 <rohitomni2125@gmail.com>
2026-09-09 21:35:46 +0530

    LC-majority-element-ii [Runtime: N/A; Memory: N/A]

--- array/majority-element-ii.cpp
+++ array/majority-element-ii.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ls;
+    //     vector<int> ls;
+    //     int n = nums.size();
+    //     int num = n/3;
+    //     // cout<<ls.size();
+    //     for(int i =0; i<n; i++){
+    //         if(ls.size() == 0 || ls[0]!=nums[i]){
+    //             int count  = 0;
+    //             for(int j =0; j<n; j++){
+    //                 if(nums[j] == nums[i]){
+    //                     count++;
+    //                 }
+    //             }
+    //             if(count>num){
+    //                 ls.push_back(nums[i]);
+    //             }
+    //         }
+    //         if(ls.size() == 2) break;            
+    //     }
+    //     return ls;       
+    // }this was the optimal appraoch for it
+        unordered_map<int, int> mpp;
+        vector<int> answer;
         int n = nums.size();
-        int num = n/3;
-        // cout<<ls.size();
-        for(int i =0; i<n; i++){
-            if(ls.size() == 0 || ls[0]!=nums[i]){
-                int count  = 0;
-                for(int j =0; j<n; j++){
-                    if(nums[j] == nums[i]){
-                        count++;
-                    }
-                }
-                if(count>num){
-                    ls.push_back(nums[i]);
-                }
+        int m = (n/3)+1;
+        for(int i =0;i <n; i++){
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]==m){
+                answer.push_back(nums[i]);
             }
-            if(ls.size() == 2) break;            
         }
-        return ls;       
+    return answer;
     }
 };
