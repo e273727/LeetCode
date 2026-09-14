53011f62c4dcc32b3a6372b5a9d946191821b746
e273727 <rohitomni2125@gmail.com>
2026-09-14 22:56:07 +0530

    LC-rectangle-overlap [Runtime: N/A; Memory: N/A]

--- geometry/rectangle-overlap.cpp
+++ geometry/rectangle-overlap.cpp
@@ -3,7 +3,7 @@ public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
         int len1 = abs(rec1[0]-rec1[2]);
         int len2 = abs(rec2[0]-rec2[2]);
-        if(rec2[0]-rec1[0] <len1){
+        if(rec2[0]-rec1[0] <len1 && rec2[0]-rec1[0] <len2 ){
             return true;
         }
         return false;        
