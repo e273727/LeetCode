bc5aec681479611709e6325bcf81786f9ba6fdee
e273727 <rohitomni2125@gmail.com>
2026-09-16 01:36:45 +0530

    LC-3sum-closest [Runtime: N/A; Memory: N/A]

--- two-pointers/3sum-closest.cpp
+++ two-pointers/3sum-closest.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        set<int> s;
-        for(int i =0; i<nums.size(); i++){
-            for(int j = i+1; j<nums.size(); j++){
-                for(int k = j+1; k<nums.size(); k++){
-                    int sum = nums[i]+nums[j]+nums[k];
-                    s.insert(sum);
+        int n = nums.size();
+        int close = nums[0] + nums[1] + nums[2];
+
+        sort(nums.begin(), nums.end());
+
+        for (int i{}; i < n-2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (abs(sum - target) < abs(close - target)) {
+                    close = sum;
+                }
+
+                if (sum == target) {
+                    return sum;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
                 }
             }
         }
-        if (s.empty()) return -1;
 
-        auto it = s.lower_bound(target);
-
-        if (it == s.begin()) return *it;
-        if (it == s.end()) return *prev(it);
-
-        int val1 = *it;
-        int val2 = *prev(it);
-        return (abs(val1 - target) < abs(val2 - target)) ? val1 : val2;
+        return close;
     }
 };
